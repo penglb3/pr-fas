f224c6be7bafd2b3e22de7a3073291c97e90093a
penglb3 <penglb3@mail2.sysu.edu.cn>
2023-03-20 15:32:22 +0800

    test bench input and output.

--- README.md
+++ README.md
@@ -9,6 +9,12 @@ PageRank FAS
 
 (If you have `ninja-build` installed, you can do `cmake -S . -B build -G Ninja` for the first step to speed things up.)
 
+## Run
+`./build/test_bench [-s greedy|greedy_opt|sort|page_rank] [-i <input_file_path>]`
+Parameters:
+- `-s`: Specify solver. Optional. Default value = `page_rank`
+- `-i`: Specify input dataset file path. Optional. Default = use standatd small graph from TA's slides.
+
 ## Test Environments
 We tested and ran the programs under these environments:
 
@@ -16,6 +22,3 @@ We tested and ran the programs under these environments:
 - OS: Ubuntu 22.04
 - Compiler: GCC 11.3.0
 - CMake: 3.22.1
-
-## Run
-`./build/test_bench -s [greedy|sort|page_rank]`
--- src/test_bench.cc
+++ src/test_bench.cc
@@ -1,8 +1,9 @@
 #include "common.h"
 #include <algorithm>
+#include <cstdio>
+#include <ctime>
 #include <string>
 #include <unistd.h>
-
 using std::string;
 
 class InputParser {
@@ -32,41 +33,89 @@ private:
   std::vector<std::string> tokens_;
 };
 
+auto read_input(const string &filename) -> std::pair<SparseMatrix, int> {
+  if (filename.empty()) { // Use standard example from TA's PPT.
+    SparseMatrix mat(7);
+    add_edge(mat, 0, 1);
+    add_edge(mat, 1, 2);
+    add_edge(mat, 2, 3);
+    add_edge(mat, 3, 0);
+    add_edge(mat, 3, 1);
+    add_edge(mat, 4, 5);
+    add_edge(mat, 5, 6);
+    add_edge(mat, 6, 4);
+    return {mat, 8};
+  }
+  // TODO(We need to know the dataset format!)
+  FILE *file = fopen(filename.c_str(), "r");
+  if (file == nullptr) {
+    printf("File '%s' doesn't exist.\n", filename.c_str());
+    return {};
+  }
+  int size, from, to, n_edges = 0;
+  int n_scanned = fscanf(file, "%d", &size);
+  if (n_scanned != 1) {
+    printf("Can't read graph size\n");
+    return {};
+  }
+  SparseMatrix mat(size);
+  while (n_scanned != EOF) {
+    n_scanned = fscanf(file, "%d %d", &from, &to);
+    switch (n_scanned) {
+    case 2:
+      add_edge(mat, from, to);
+      n_edges++;
+      break;
+    case EOF:
+      continue;
+    default:
+      puts("Input pattern mismatch");
+      return {};
+    }
+  }
+  return {mat, n_edges};
+}
+
 // test_bench.cc
 int main(int argc, const char *argv[]) {
-  fas_solver solver_function;
   InputParser parser(argc, argv);
-  constexpr int size = 7;
-  SparseMatrix mat(size);
 
-  // Use standard example from TA's PPT.
-  add_edge(mat, 0, 1);
-  add_edge(mat, 1, 2);
-  add_edge(mat, 2, 3);
-  add_edge(mat, 3, 0);
-  add_edge(mat, 3, 1);
-
-  add_edge(mat, 4, 5);
-  add_edge(mat, 5, 6);
-  add_edge(mat, 6, 4);
-
+  // Default = PageRank
+  fas_solver solver_function = page_rank_fas;
+  string solver_name = "page_rank";
   if (parser.option_exists("-s")) {
-    string solver_name = parser.get_option("-s");
+    solver_name = parser.get_option("-s");
     if (solver_name == "greedy") {
       solver_function = greedy_fas;
+    } else if (solver_name == "greedy_opt") {
+      solver_function = greedy_fas; // @hxt
     } else if (solver_name == "sort") {
       solver_function = sort_fas;
-    } else if (solver_name == "page_rank") {
-      solver_function = page_rank_fas;
-    } else {
+    } else if (solver_name != "page_rank") {
       return -1;
     }
-  } else {
-    std::puts("test_bench should be called via -s solver option.");
-    return 0;
   }
+  printf("Solver: %s\n", solver_name.c_str());
+
+  string input_file = parser.get_option("-i");
+  const auto [mat, n_edges] = read_input(input_file);
+  if (mat.empty() || n_edges == 0) {
+    puts("Read input failed. Abort.");
+    return -1;
+  }
+  printf("Testing graph has %lu vertices and %d edges\n", mat.size(), n_edges);
+
+  printf("Solving start...");
+  clock_t start = clock();
   FAS result = solver_function(mat);
-  // Do some validation and test.
+  clock_t end = clock();
+  puts("Done.");
+
+  float time_ms = (static_cast<float>(end - start) / CLOCKS_PER_SEC * 1000);
+  printf("Time Elapsed: %.3f(ms). \n", time_ms);
+
+  float fas_percentage = result.size() * static_cast<float>(100) / n_edges;
+  printf("Result FAS size = %lu (%.2f%%)\n", result.size(), fas_percentage);
 
   return 0;
 }
